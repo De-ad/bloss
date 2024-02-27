40111dbd1c7f62c6220152e8edb3747cdc453efa
Dead <space.gee.z@gmail.com>
2024-02-27 19:00:43 +0300

    fix userDetails error

--- src/main/java/com/brigada/bloss/BlossApplication.java
+++ src/main/java/com/brigada/bloss/BlossApplication.java
@@ -2,8 +2,14 @@ package com.brigada.bloss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.brigada.listening")
+@EntityScan("com.brigada.entity")
+@EnableJpaRepositories("com.brigada.dao")
 public class BlossApplication {
 
 	public static void main(String[] args) {
