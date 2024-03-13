fd6656d398ec4b142858840a76a9a8570d9e37b3
IndianMax03 <vermamanu@mail.ru>
2024-03-13 04:33:09 +0300

    Update: controllers

--- src/main/java/com/brigada/bloss/controller/ReviewController.java
+++ src/main/java/com/brigada/bloss/controller/ReviewController.java
@@ -1,8 +1,12 @@
 package com.brigada.bloss.controller;
 
+import com.brigada.bloss.listening.MessageResponse;
 import com.brigada.bloss.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,19 +36,44 @@ public class ReviewController {
         return reviewService.getReview(id);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping()
-    public ResponseEntity<Object> createReview(@RequestBody ReviewRequest reviewRequest) {
-        return reviewService.createReview(reviewRequest);
+    public ResponseEntity<Object> createReview(@RequestBody ReviewRequest reviewRequest, @AuthenticationPrincipal UserDetails userDetails) {
+        return reviewService.createReview(reviewRequest, userDetails.getUsername());
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping()
-    public ResponseEntity<Object> editReview(@RequestBody Review review) {
-        return reviewService.editReview(review);
+    public ResponseEntity<Object> editReview(@RequestBody Review review, @AuthenticationPrincipal UserDetails userDetails) {
+        return reviewService.checkTarget(userDetails.getUsername(), review.getId())
+                ? reviewService.editReview(review)
+                : ResponseEntity.status(403).body(new MessageResponse("Access denied"));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteReview(@PathVariable Integer id) {
-        return reviewService.deleteReview(id);
+    public ResponseEntity<Object> deleteReview(@PathVariable Integer id, @AuthenticationPrincipal UserDetails userDetails) {
+        return reviewService.checkTarget(userDetails.getUsername(), id)
+                ? reviewService.deleteReview(id)
+                : ResponseEntity.status(403).body(new MessageResponse("Access denied"));
+    }
+
+    @GetMapping("/raw")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Object> getRawReviews() {
+        return reviewService.getRawReviews();
+    }
+
+    @PostMapping("/{id}/approve")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Object> approveReview(@PathVariable  Integer id) {
+        return reviewService.approveReview(id);
+    }
+
+    @PostMapping("/{id}/reject")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Object> rejectReview(@PathVariable  Integer id) {
+        return reviewService.rejectReview(id);
     }
 
 
