e23a50155d0396c84de4b91cd1ddc8b28f1fcd7f
IndianMax03 <vermamanu@mail.ru>
2024-04-07 22:57:05 +0300

    Fix: annotation in admin controller

--- src/main/java/com/brigada/bloss/controller/AdminController.java
+++ src/main/java/com/brigada/bloss/controller/AdminController.java
@@ -7,15 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.brigada.bloss.service.AdminService;
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 @Slf4j
 public class AdminController {
